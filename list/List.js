179ec608b84513ee9e763429065fa5e28c0a3d95
Kevin Harbal <kevin.harbal@fr.ibm.com>
2015-03-31 16:16:22 +0200

    modifications on List and PageableList to work with ObservableArray + samples and automatic tests

--- list/List.js
+++ list/List.js
@@ -414,16 +414,6 @@ define([
 		//////////// delite/Selection implementation ///////////////////////////////////////
 
 		/**
-		 * Returns the identity of an item.
-		 * @param {Object} item The item
-		 * @returns {Object}
-		 * @protected
-		 */
-		getIdentity: function (item) {
-			return this.store.getIdentity(item);
-		},
-
-		/**
 		 * Updates renderers when the selection has changed.
 		 * @param {Object[]} items The items which renderers must be updated.
 		 * @protected
