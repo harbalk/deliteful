7c6f0950dd7c62267d9d22d2902a259cac7e065c
Kevin Harbal <kevin.harbal@fr.ibm.com>
2015-07-30 11:35:08 +0200

    only unit tests

--- tests/functional/all.js
+++ tests/functional/all.js
@@ -1,16 +1,16 @@
 // Listing of all the deliteful functional tests
 define([
-	"./StarRating",
-	"./ScrollableContainer",
-	"./list/all",
-	"./Toaster",
-	"./Select",
-	"./Slider",
-	"./SidePane",
-	"./Checkbox",
-	"./Switch",
-	"./Button",
-	"./Combobox",
-	"./SwapView",
-	"./features"
+	//"./StarRating",
+	//"./ScrollableContainer",
+	//"./list/all",
+	//"./Toaster",
+	//"./Select",
+	//"./Slider",
+	//"./SidePane",
+	//"./Checkbox",
+	//"./Switch",
+	//"./Button",
+	//"./Combobox",
+	//"./SwapView",
+	//"./features"
 ]);
